307cf3b44a2a6ddcc4f531e6afe1e363a8c6905a
Bo4anckii <bobaanton@gmail.com>
2020-11-14 17:42:38 +0300

    Панель тестирования

--- src/client/ui/ClientController.java
+++ src/client/ui/ClientController.java
@@ -1,11 +1,10 @@
 package client.ui;
 
 import client.Client;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import models.Test;
 
 import java.net.URL;
@@ -18,6 +17,18 @@ public class ClientController implements Initializable {
     public ListView<Test> testList;
     public Button testBtn;
     public Button resultBtn;
+
+    public AnchorPane testPane;
+    public Label questionLabel;
+    public ToggleGroup variants;
+    public RadioButton var1RB;
+    public RadioButton var2RB;
+    public RadioButton var3RB;
+    public RadioButton var4RB;
+    public Button backBtn;
+    public Button doneBtn;
+    public Button forwardBtn;
+
     private final Client client = new Client();
     private final ClientModel model = new ClientModel();
 
@@ -37,6 +48,28 @@ public class ClientController implements Initializable {
 
         });
         resultBtn.setOnAction(event -> {
+
+        });
+
+        var1RB.setOnAction(event -> {
+
+        });
+        var2RB.setOnAction(event -> {
+
+        });
+        var3RB.setOnAction(event -> {
+
+        });
+        var4RB.setOnAction(event -> {
+
+        });
+        backBtn.setOnAction(event -> {
+
+        });
+        forwardBtn.setOnAction(event -> {
+
+        });
+        doneBtn.setOnAction(event -> {
 
         });
     }
