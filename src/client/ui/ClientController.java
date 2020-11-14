2f775da7166eec6cfe6d109aec1fdd1e96630c2f
Bo4anckii <bobaanton@gmail.com>
2020-11-14 11:05:13 +0300

    Подключение клиентов

--- src/client/ui/ClientController.java
+++ src/client/ui/ClientController.java
@@ -1,13 +1,17 @@
-package client;
+package client.ui;
 
+import client.Client;
 import javafx.fxml.Initializable;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClientController implements Initializable {
 
+    private final Client client = new Client();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        client.start();
     }
 }
--- src/server/ui/ServerController.java
+++ src/server/ui/ServerController.java
@@ -1,13 +1,17 @@
-package server;
+package server.ui;
 
 import javafx.fxml.Initializable;
+import server.RequestListener;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ServerController implements Initializable {
 
+    private final RequestListener server = new RequestListener();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        server.start();
     }
 }
